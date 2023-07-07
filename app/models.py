23f5f81b58e5b55c9c05dcafe48e58ac0ad09ff1
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-07 23:28:40 +0300

    add Rubrics, Document models

--- app/models.py
+++ app/models.py
@@ -1,10 +1,38 @@
 """ database model """
+import datetime
+
+from sqlalchemy import (
+    Text,
+    Integer,
+    ForeignKey,
+    String,
+    DateTime
+)
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     """ Base class """
-    
+
     pass
 
 
+class Rubrics(Base):
+    """ Rubrics model """
+    __tablename__ = 'rubrics'
+
+    document_id: Mapped[int] = mapped_column(ForeignKey('documents.id'))
+    rubrics: Mapped[str] = mapped_column(String(40))
+
+
+class Document(Base):
+    """ Document model """
+
+    __tablename__ = 'documents'
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True)
+    text: Mapped[str] = mapped_column(Text)
+    created_date: [datetime.datetime] = mapped_column(DateTime)
