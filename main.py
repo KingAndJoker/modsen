8d55efe4449ee79bc81172db83e45523c8b2093c
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-07 23:30:16 +0300

    add run code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 """ main file """
+from aiohttp import web
+
+from setup import setup
+
+
+app: web.Application = web.Application()
+
+
+if __name__ == '__main__':
+    setup(app)
+    web.run_app(app)
