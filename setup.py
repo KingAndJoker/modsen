e738e4828299508664ca880d96d17a9e25ae8073
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-07 23:28:11 +0300

    add database setup

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 """ setup file """
 from aiohttp import web
+from sqlalchemy import create_engine
+
+from app.models import Base
+
+
+def setup_db(app: web.Application) -> None:
+    engine = create_engine("sqlite:///database.db", echo=False)
+    app['engine'] = engine
+    Base.metadata.create_all(engine)
 
 
 def setup(app: web.Application) -> None:
     """ setup web application """
 
+    setup_db(app)
     pass
