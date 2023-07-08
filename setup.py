86ef72bb2581ac77f7bc935b79593659fc996746
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-08 17:07:25 +0300

    init seeding database

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 """ setup file """
 from aiohttp import web
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 
 from app.models import Base
+
+
+def seeding(engine: Engine) -> None:
+    """ sedding database """
+
+    pass
 
 
 def setup_db(app: web.Application) -> None:
