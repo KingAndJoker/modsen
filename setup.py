d897b3d4f31c7107b483ffa24c9848c58fdb4e6b
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-08 18:34:16 +0300

    seeding def

--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
 """ setup file """
 import os
+import csv
+import datetime
+from ast import literal_eval
 
 from aiohttp import web
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from sqlalchemy.engine import Engine
 
-from app.models import Base
+from app.models import (
+    Base,
+    Rubric,
+    Document
+)
 
 
 def seeding(engine: Engine) -> None:
     """ sedding database """
+    
+    with Session(engine) as session:
+        with open('posts.csv', encoding='utf-8') as csv_file:
+            spamreader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
+            for row in spamreader:
+                text = row['text']
+                created = datetime.datetime.fromisoformat(row['created_date'])
+                rubrics = literal_eval(row['rubrics'])
+                rubrics = [Rubric(rubric=rubric) for rubric in rubrics]
 
-    pass
+                document = Document(
+                    text=text,
+                    created_date=created,
+                    rubrics=rubrics
+                )
+
+                session.add(document)
+                session.commit()
 
 
 def setup_db(app: web.Application) -> None:
