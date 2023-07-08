fb8d9e85ab76eebdb33e8e23a54df87df704a7f3
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-08 18:07:08 +0300

    add init methods

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 """ database model """
 import datetime
+from typing import Any
 
 from sqlalchemy import (
     Text,
@@ -17,7 +18,8 @@ from sqlalchemy.orm import relationship
 class Base(DeclarativeBase):
     """ Base class """
 
-    pass
+    def __init__(self, **kw: Any):
+        super().__init__(**kw)
 
 
 class Rubrics(Base):
@@ -32,7 +34,17 @@ class Rubrics(Base):
     document_id: Mapped[int] = mapped_column(ForeignKey('documents.id'))
     rubrics: Mapped[str] = mapped_column(String(40))
 
-    document: Mapped['Document'] = relationship('Document', back_populates='rubrics')
+    document: Mapped['Document'] = relationship(
+        'Document', back_populates='rubrics')
+
+    def __init__(self, id: int = None,
+                 document_id: int = None,
+                 rubrics: str = None,
+                 **kw: Any):
+        super().__init__(**kw)
+        self.id = id
+        self.document_id = document_id
+        self.rubrics = rubrics
 
 
 class Document(Base):
@@ -48,4 +60,17 @@ class Document(Base):
     text: Mapped[str] = mapped_column(Text)
     created_date: Mapped[datetime.datetime] = mapped_column(DateTime)
 
-    rubrics: Mapped[list[Rubrics]] = relationship('Rubrics', back_populates='document')
+    rubrics: Mapped[list[Rubrics]] = relationship(
+        'Rubrics', back_populates='document')
+
+    def __init__(self,
+                 id: int = None,
+                 text: str = None,
+                 created_date: datetime.datetime = None,
+                 rubrics: list[Rubrics] = None,
+                 **kw: Any):
+        super().__init__(**kw)
+        self.id = id
+        self.text = text
+        self.created_date = created_date
+        self.rubrics = rubrics
