33dd33ec8853f9b7bc96b9e1f1c5201d96daf526
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-11 21:07:40 +0300

    add postgresql

--- setup.py
+++ setup.py
@@ -1,61 +1,18 @@
 """ setup file """
-import os
-import csv
-import datetime
-from ast import literal_eval
-
 from aiohttp import web
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.engine import Engine
 
-from app.models import (
-    Base,
-    Rubric,
-    Document
-)
+from app.models import Base
 from app.api import routes as routes_api
-
-
-def seeding(engine: Engine) -> None:
-    """ seeding database """
-
-    with Session(engine) as session:
-        with open('posts.csv', encoding='utf-8') as csv_file:
-            spamreader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
-            for row in spamreader:
-                text = row['text']
-                created = datetime.datetime.fromisoformat(row['created_date'])
-                rubrics = literal_eval(row['rubrics'])
-                for i, rubric in enumerate(rubrics):
-                    r = session.query(Rubric). \
-                        filter(Rubric.rubric == rubric). \
-                        one_or_none()
-                    if r is None:
-                        r = Rubric(rubric=rubric)
-                    rubrics[i] = r
-
-                document = Document(
-                    text=text,
-                    created_date=created,
-                    rubrics=rubrics
-                )
-
-                session.add(document)
-                session.commit()
 
 
 def setup_db(app: web.Application) -> None:
     """ setup database """
 
-    required_seeding = not os.path.isfile('./database.db')
-
-    engine = create_engine("sqlite:///database.db", echo=False)
+    engine = create_engine("postgresql+psycopg2://modsen_practica:1@localhost:5432", echo=False)
     app['engine'] = engine
     Base.metadata.create_all(engine)
-
-    if required_seeding:
-        seeding(engine)
 
     app['session'] = Session(engine)
 
