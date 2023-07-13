4cdd4ec44b5f3389ae6d448c2021f7d7875bc04a
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-13 20:47:50 +0300

    add schema

--- app/schema.py
+++ app/schema.py
@@ -1,9 +1,18 @@
 """ Schema file """
 from marshmallow import Schema, fields
 
+from app.models import (
+    Rubric,
+    Document
+)
+
 
 class RubricSchema(Schema):
     """ Rubric schema """
+
+    class Meta:
+        model = Rubric
+        fields = ('id', 'rubric')
 
     id = fields.Int()
     rubric = fields.Str()
@@ -12,7 +21,12 @@ class RubricSchema(Schema):
 class DocumentSchema(Schema):
     """ Document schema """
 
+    class Meta:
+        model = Document
+        fields = ('id', 'text', 'created_date', 'rubrics')
+        ordered = True
+
     id = fields.Int()
     text = fields.Str()
     created_date = fields.DateTime()
-    rubric = fields.Nested(RubricSchema, many=True)
+    rubrics = fields.Nested(RubricSchema(), many=True)
