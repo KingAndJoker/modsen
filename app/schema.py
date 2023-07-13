0228b5a59d69b33247faa6d06809d2bc7201ec6b
KingAndJoker <a12345678.87654321@yandex.ru>
2023-07-13 16:35:34 +0300

    add schema

--- app/schema.py
+++ app/schema.py
@@ -1,1 +1,18 @@
 """ Schema file """
+from marshmallow import Schema, fields
+
+
+class RubricSchema(Schema):
+    """ Rubric schema """
+
+    id = fields.Int()
+    rubric = fields.Str()
+
+
+class DocumentSchema(Schema):
+    """ Document schema """
+
+    id = fields.Int()
+    text = fields.Str()
+    created_date = fields.DateTime()
+    rubric = fields.Nested(RubricSchema, many=True)
